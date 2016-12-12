e499190ad1fdaeff1cae8051306adbf10c8d6877
Juuso <juuso.soikkeli@gmail.com>
2016-12-12 17:09:32 +0200

    github link now opens in new tab

--- src/js/App.js
+++ src/js/App.js
@@ -86,7 +86,7 @@ export default class App extends Component {
             <h2 style={{ userSelect: 'none' }}>Pixel Studio</h2>
           </div>
           <div className="two columns">
-            <a href="https://github.com/Straiffi/pixel-studio" title="View on GitHub"><i className="icon-github-circled" style={{ color: '#000', fontSize: 45 }}></i></a>
+            <a href="https://github.com/Straiffi/pixel-studio" title="View on GitHub" target="_blank"><i className="icon-github-circled" style={{ color: '#000', fontSize: 45 }}></i></a>
           </div>
         </div>
         <div className="row center margin-bottom">
